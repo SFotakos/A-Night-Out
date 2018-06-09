c38608fb98efe02729065e72e215819e63b25925
SFotakos <s.fotakos@gmail.com>
2018-06-09 19:35:40 -0300

    Removes home tab icons

--- app/src/main/java/sfotakos/anightout/home/HomeActivity.java
+++ app/src/main/java/sfotakos/anightout/home/HomeActivity.java
@@ -2,7 +2,6 @@ package sfotakos.anightout.home;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 
 import sfotakos.anightout.R;
@@ -24,21 +23,5 @@ public class HomeActivity extends AppCompatActivity {
                 new HomeNavigationFragmentPagerAdapter(getSupportFragmentManager()));
 
         mBinding.homeTabLayout.setupWithViewPager(mBinding.homeViewPager);
-        setupTabIcons();
-    }
-
-    private void setupTabIcons () {
-        for (int i = 0; i < mBinding.homeTabLayout.getTabCount(); i++) {
-            TabLayout.Tab tab = mBinding.homeTabLayout.getTabAt(i);
-            if (tab != null) {
-                if (i == 0){
-                    tab.setIcon(R.drawable.ic_events);
-                } else if (i == 1){
-                    tab.setIcon(R.drawable.ic_map);
-                } else {
-                    throw new RuntimeException();
-                }
-            }
-        }
     }
 }
